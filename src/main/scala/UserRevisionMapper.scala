00b2aef9fb67530da6744ef22d9686f79b70984f
Rastal <rastal@protonmail.com>
2020-12-31 10:05:34 -0800

    Set up mapper class for user revisions
    Also saved a copy of the schema so I can remember which columns certain fields are in

--- src/main/scala/UserRevisionMapper.scala
+++ src/main/scala/UserRevisionMapper.scala
@@ -1,16 +1,16 @@
 import org.apache.hadoop.mapreduce.Mapper
 import org.apache.hadoop.io.{IntWritable, Text}
 
-class PageRevisionMapper extends Mapper[Object, Text, Text, IntWritable] {
+class UserRevisionMapper extends Mapper[Object, Text, Text, IntWritable] {
   override def map(
-    key: Object, 
-    value: Text, 
+    key: Object,
+    value: Text,
     context: Mapper[Object, Text, Text, IntWritable]#Context
   ): Unit = {
     val line = value.toString
     val fields = line.split('\t')
     if (fields(1) == "revision") {
-      context.write(new Text(fields(25)), new IntWritable(1))
+      context.write(new Text(fields(7)), new IntWritable(1))
     }
   }
 }
