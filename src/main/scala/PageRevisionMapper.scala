99802f218da0c02811f6c62ad12d1efbadb2c604
Rastal <rastal@protonmail.com>
2020-12-31 09:57:46 -0800

    Set up mapper class for wikipedia page revisions

---
+++ src/main/scala/PageRevisionMapper.scala
@@ -0,0 +1,16 @@
+import org.apache.hadoop.mapreduce.Mapper
+import org.apache.hadoop.io.{IntWritable, Text}
+
+class PageRevisionMapper extends Mapper[Object, Text, Text, IntWritable] {
+  override def map(
+    key: Object, 
+    value: Text, 
+    context: Mapper[Object, Text, Text, IntWritable]#Context
+    ): Unit = {
+      val line = value.toString()
+      val fields = line.split('\t')
+      if (fields(1) == "revision") {
+        context.write(new Text(fields(24)), new IntWritable(1))
+      }
+  }
+}
