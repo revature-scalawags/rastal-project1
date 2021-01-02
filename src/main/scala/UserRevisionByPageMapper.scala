644d14c6ba3f8d70c4fa989385963513a4a87382
Rastal <rastal@protonmail.com>
2021-01-02 08:24:20 -0800

    Change output of UserRevisionByPageMapper
    From MapWritable to Text (aka let the reducer handle the counting and mapping)

--- src/main/scala/UserRevisionByPageMapper.scala
+++ src/main/scala/UserRevisionByPageMapper.scala
@@ -3,19 +3,17 @@ package me.rastal.project1
 import org.apache.hadoop.mapreduce.Mapper
 import org.apache.hadoop.io.{IntWritable, MapWritable, Text}
 
-class UserRevisionByPageMapper extends Mapper[Object, Text, Text, MapWritable] {
+class UserRevisionByPageMapper extends Mapper[Object, Text, Text, Text] {
   override def map(key: Object,
                    value: Text,
-                   context: Mapper[Object, Text, Text, MapWritable]#Context): Unit = {
+                   context: Mapper[Object, Text, Text, Text]#Context): Unit = {
     val line = value.toString
     val fields = line.split('\t')
     val event = fields(1)
     val title = fields(25)
     if (event == "revision" && title.length > 0) {
-      val map = new MapWritable()
       val user = new Text(fields(7))
-      map.put(user, new IntWritable(1))
-      context.write(new Text(title), map)
+      context.write(new Text(title), user)
     }
   }
 }
