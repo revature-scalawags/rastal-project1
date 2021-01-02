a1e8a466659d0a03f789bed74132503432ff93cf
Rastal <rastal@protonmail.com>
2021-01-02 08:35:15 -0800

    Add CLI options for new mapper and reducer
    Third arg can now be 'usersByPage' to mapreduce based on number of revisions by user for each page

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,14 +1,11 @@
 package me.rastal.project1
 
-import java.lang
-import java.util.StringTokenizer
-
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.Path
-import org.apache.hadoop.io.{IntWritable, Text}
+import org.apache.hadoop.io.{IntWritable, MapWritable, Text}
+import org.apache.hadoop.mapreduce.Job
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat
-import org.apache.hadoop.mapreduce.{Job, Mapper, Reducer}
 
 import scala.jdk.CollectionConverters._
 
@@ -21,11 +18,19 @@ object Main extends App {
   args(2) match {
     case "pages" => job.setMapperClass(classOf[PageRevisionMapper])
     case "users" => job.setMapperClass(classOf[UserRevisionMapper])
+    case "usersByPage" => job.setMapperClass(classOf[UserRevisionByPageMapper])
   }
-  job.setCombinerClass(classOf[RevisionReducer])
-  job.setReducerClass(classOf[RevisionReducer])
+  args(2) match {
+    case "pages" | "users" =>
+      job.setCombinerClass(classOf[RevisionReducer])
+      job.setReducerClass(classOf[RevisionReducer])
+      job.setOutputValueClass(classOf[IntWritable])
+    case "usersByPage" => 
+      job.setCombinerClass(classOf[UserRevisionByPageReducer])
+      job.setReducerClass(classOf[UserRevisionByPageReducer])
+      job.setOutputValueClass(classOf[MapWritable])
+  }
   job.setOutputKeyClass(classOf[Text])
-  job.setOutputValueClass(classOf[IntWritable])
   FileInputFormat.addInputPath(job, new Path(args(0)))
   FileOutputFormat.setOutputPath(job, new Path(args(1)))
   System.exit(if(job.waitForCompletion(true)) 0 else 1)
@@ -33,7 +38,8 @@ object Main extends App {
   def processArgs(args: Array[String], test: Boolean = false): Unit = {
     if (args.length != 3 || !(args(2) == "users" || args(2) == "pages")) {
       if (!test) {
-        println("Usage: Main [input dir] [output dir] ['pages'/'users']")
+        println("Usage: Main [input dir] [output dir] [job type]\n" +
+                "Job types: 'pages' 'users' 'usersByPage'")
         System.exit(-1)
       } else {
         // TODO actually fill out stuff that could test this
